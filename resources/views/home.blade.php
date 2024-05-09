c60320425b92fc8a683a42a8120ff1086c3b8ce4
M. Agung Wibowo <72590651+agungwibowo182@users.noreply.github.com>
2024-05-09 19:56:24 +0700

    add templating
    add templating

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('title')
+    Dashboard
+@endsection
+
+@section('breadcrumb')
+    @parent
+    <li class="active">Dashboard</li>
+@endsection
+
+@section('content')
+  Selamat datang di Aplikasi Pos
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\KategoriController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect()->route('login');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->get('dashboard', function() {
+    return view('home');
+})-> name('dashboard');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/kategori/data', [KategoriController::class, 'data']);
+    Route::resource('/kategori', KategoriController::class);
+});
